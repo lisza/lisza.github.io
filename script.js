87ce60414bf83ea3bf6e658826547af62805097c
lisza <liszalisza@gmail.com>
2017-04-24 01:06:51 -0700

    Add script.js and color transition

---
+++ script.js
@@ -0,0 +1,18 @@
+// Gives a random color to the element specified as a parameter
+function assignRandomColor() {
+  var randomColor = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) + ')';
+  document.body.style.background = randomColor;
+};
+
+// Returns a random number between and incl parameters min and max
+function random(min,max) {
+  var num = Math.floor(Math.random()*(max-min)) + min;
+  return num;
+};
+
+function changeFontColor() {
+  var randomColor = 'rgb(' + random(0,255) + ',' + random(0,255) + ',' + random(0,255) + ')';
+  document.getElementById('hi').style.color = randomColor;
+};
+
+setInterval(changeFontColor, 10000);
